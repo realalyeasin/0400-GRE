3c2b204bc8e91946b90c187b1438288e7b60c584
Yeasin <realalyeasin@gmail.com>
2023-02-25 19:05:09 +0600

    v2

---
+++ lib/model/model.dart
@@ -0,0 +1,76 @@
+class WordClass {
+  int? id;
+  String? word;
+  String? meaning;
+  String? example;
+  String? syn;
+  String? ant;
+  String? pos;
+  int? freq;
+  int? fvrt;
+
+  WordClass(
+      {this.id,
+        this.word,
+        this.meaning,
+        this.example,
+        this.syn,
+        this.ant,
+        this.pos,
+        this.freq,
+        this.fvrt});
+
+  WordClass.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    word = json['word'];
+    meaning = json['meaning'];
+    example = json['example'];
+    syn = json['syn'];
+    ant = json['ant'];
+    pos = json['pos'];
+    freq = json['freq'];
+    fvrt = json['fvrt'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['id'] = this.id;
+    data['word'] = this.word;
+    data['meaning'] = this.meaning;
+    data['example'] = this.example;
+    data['syn'] = this.syn;
+    data['ant'] = this.ant;
+    data['pos'] = this.pos;
+    data['freq'] = this.freq;
+    data['fvrt'] = this.fvrt;
+    return data;
+  }
+
+  WordClass.fromMap(Map<String, dynamic> map) {
+    id = map['id'];
+    word = map['word'];
+    meaning = map['meaning'];
+    example = map['example'];
+    syn = map['syn'];
+    ant = map['ant'];
+    pos = map['pos'];
+    freq = map['freq'];
+    fvrt = map['fvrt'];
+  }
+
+  Map<String, dynamic> toMap() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['id'] = this.id;
+    data['word'] = this.word;
+    data['meaning'] = this.meaning;
+    data['example'] = this.example;
+    data['syn'] = this.syn;
+    data['ant'] = this.ant;
+    data['pos'] = this.pos;
+    data['freq'] = this.freq;
+    data['fvrt'] = this.fvrt;
+    return data;
+  }
+
+
+}
--- lib/views/readcsv.dart
+++ lib/views/readcsv.dart
@@ -6,10 +6,12 @@ import 'package:flutter/material.dart';
 import 'package:csv/csv.dart';
 import 'package:flutter/services.dart';
 import 'package:get/get.dart';
-import 'package:sort/sort.dart';import 'package:path/path.dart';
-
-
+import 'package:sort/sort.dart';
+import 'package:path/path.dart';
+import 'package:flutter/services.dart' as rootBundle;
+import '../model/model.dart';
 import '../practice.dart';
+
 class ReadCSV extends StatefulWidget {
   const ReadCSV({Key? key}) : super(key: key);
 
@@ -18,63 +20,120 @@ class ReadCSV extends StatefulWidget {
 }
 
 class _ReadCSVState extends State<ReadCSV> {
-
   List<List<dynamic>> data = [];
-  var inx=1.obs;
+  var inx = 1.obs;
   var isSelected = false;
   var loadDone = false.obs;
-
+  var favorite = false.obs;
+  var listIndex = false.obs;
+  var tappedIndex = -100.obs;
+  var tappedListIndex = -100.obs;
 
   @override
   initState() {
     // ignore: avoid_print
     print("initState Called");
     loadAsset();
+    createDB();
     print('Loaded');
   }
+
   loadAsset() async {
-    final myData = await rootBundle.loadString("assets/word.csv");
+    final myData = await rootBundle.rootBundle.loadString("assets/word.csv");
     List<List<dynamic>> csvTable = const CsvToListConverter().convert(myData);
     data = csvTable;
     print(data);
     loadDoneCheck();
   }
-  loadDoneCheck(){
+
+  loadDoneCheck() {
     loadDone.toggle();
   }
+
   createDB() async {
     Database database = await openDatabase(data as String, version: 1,
         onCreate: (Database db, int version) async {
-          await db.execute(
-              'CREATE TABLE Test (id INTEGER PRIMARY KEY, name TEXT, value INTEGER, num REAL)');
-        });
+      var words = 'words';
+      await db.execute("""CREATE TABLE $words(
+        id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+        word TEXT,
+        meaning TEXT,
+        example TEXT,
+        syn TEXT,
+        ant TEXT,
+        pos TEXT,
+        freq INTEGER,
+        fvrt INTEGER)""");
+
+      Batch batch = db.batch();
+      final wordJson = await rootBundle.rootBundle.loadString('assets/wordjson.json');
+      final wordList = json.decode(wordJson) as List<dynamic>;
+      wordList.forEach((val) {
+        WordClass word = WordClass.fromMap(val);
+        batch.insert(words, word.toMap());
+        print(wordList);
+        print('WORDLIST');
+      });
+
+      batch.commit();
+    });
   }
 
   Widget build(BuildContext context) {
-
-    return  Scaffold(
+    return Scaffold(
       drawer: Drawer(
         child: Column(
           children: [
-            Container(color: Colors.cyan,child: Image.network('https://tse3.mm.bing.net/th?id=OIP.mSxhoiAZ8nC4DkI05_9PNAEsCy&pid=Api&P=0'),),
-            FlatButton(onPressed: (){
-
-            }, child: Text('High Frequency Words')),
+            Container(
+                color: Colors.cyan,
+                child: Image.asset('images/drawerImage.jpg')),
+            SizedBox(
+              height: 15,
+            ),
+            Text(
+              'GRE Vocabulary',
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 22),
+            ),
+            SizedBox(
+              height: 3,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                color: Colors.black,
+                height: 2,
+              ),
+            ),
             Container(
               child: Column(
                 children: [
-
-                  SizedBox(height: 35,),
+                  FlatButton(
+                      onPressed: () {}, child: Text('High Frequency Words')),
+                  SizedBox(
+                    height: 1,
+                  ),
                   Text('All Words'),
-                  SizedBox(height: 15,),
+                  SizedBox(
+                    height: 15,
+                  ),
                   Text('Favorite Words'),
                   //SizedBox(height: 15,),
                   //Text('High Frequency Words'),
-                  SizedBox(height: 15,),
-                  GestureDetector(onTap: (){Get.to(()=>Practice());},child: Text('Practice Session')),
-                  SizedBox(height: 15,),
+                  SizedBox(
+                    height: 15,
+                  ),
+                  GestureDetector(
+                      onTap: () {
+                        Get.to(() => Practice());
+                      },
+                      child: Text('Practice Session')),
+                  SizedBox(
+                    height: 15,
+                  ),
                   Text('Feedback'),
-                  SizedBox(height: 15,),
+                  SizedBox(
+                    height: 15,
+                  ),
                   Text('Contact Us'),
                 ],
               ),
@@ -82,63 +141,187 @@ class _ReadCSVState extends State<ReadCSV> {
           ],
         ),
       ),
-      appBar: AppBar(title: const Text('CSV Read'),),
+      appBar: AppBar(
+        iconTheme: IconThemeData(color: Colors.black),
+        backgroundColor: Colors.white,
+        title: const Text(
+          'GRE Vocabularies',
+          style: TextStyle(
+              fontStyle: FontStyle.italic,
+              color: Colors.black,
+              fontWeight: FontWeight.bold,
+              letterSpacing: 2),
+        ),
+      ),
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          Obx(()=> SingleChildScrollView(
-              child: loadDone.isTrue ?   ListView.builder(
-                shrinkWrap: true,
-              itemCount: data.length
-              ,itemBuilder: (_, index){
-                return Padding(padding: const EdgeInsets.all(8.0) , child : GestureDetector(
-                  onTap: (){
-
-                    setState(() {
-                      inx = index.obs;
-                      print(inx);
-                      print(data[inx.value][3].toString());
-                    });
-                  },
+          Obx(() => loadDone.isTrue
+              ? Padding(
+                  padding: const EdgeInsets.all(12.0),
                   child: Container(
-                    color: isSelected ? Colors.cyan : Colors.lightGreen,
-                    child: GestureDetector(
-                      onTap: (){isSelected = true;},
-                      child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Text(data[index][1].toString()),
-                            Text("  --  "),
-                            Container(width: 10,),
-                            Text(data[index][2].toString()),
-                          ],
+                    height: 200,
+                    width: 400,
+                    decoration: BoxDecoration(
+                        border: Border.all(color: Colors.black, width: 3),
+                        color: const Color.fromRGBO(126, 212, 230, 1),
+                        borderRadius: BorderRadius.circular(8)),
+                    child: Column(
+                      children: [
+                        Text(
+                          data[inx.value][1].toString(),
+                          style: const TextStyle(fontSize: 19),
                         ),
-                      ),
-                    ),),
+                        Text(
+                          data[inx.value][2].toString(),
+                          style: TextStyle(fontSize: 19),
+                        ),
+                        Text(
+                          data[inx.value][3].toString(),
+                          style: TextStyle(fontSize: 19),
+                          textAlign: TextAlign.start,
+                        ),
+                        Text(
+                          data[inx.value][4].toString(),
+                          style: TextStyle(fontSize: 19),
+                        ),
+                        Text(
+                          data[inx.value][5].toString(),
+                          style: TextStyle(fontSize: 19),
+                        ),
+                        Text(
+                          data[inx.value][6].toString(),
+                          style: const TextStyle(fontSize: 19),
+                        ),
+                        Text(
+                          data[inx.value][7].toString(),
+                          style: const TextStyle(fontSize: 19),
+                        ),
+                      ],
+                    ),
+                  ),
                 )
-                );
-              }) : Center(child: CircularProgressIndicator(),) ),
-          )
-          ,
-          Obx (()=> loadDone.isTrue ? Padding(
+              : Container()),
+          Padding(
             padding: const EdgeInsets.all(8.0),
             child: Container(
-              height: 110,
-              width: 400,
-              color: Colors.orange,
-              child:  Column(
-                children: [
-                  Text(data[inx.value][1].toString()),
-                  Text(data[inx.value][2].toString()),
-                  Text(data[inx.value][3].toString()),
-                  Text(data[inx.value][4].toString()),
-                  Text(data[inx.value][6].toString()),
-                ],
-              ),),
-          ): Container()
-          )
+              color: Colors.black,
+              height: 2,
+            ),
+          ),
+          Obx(
+            () => SingleChildScrollView(
+                child: loadDone.isTrue
+                    ? Container(
+                        height: MediaQuery.of(context).size.height - 315,
+                        child: ListView.builder(
+                            shrinkWrap: true,
+                            itemCount: data.length,
+                            itemBuilder: (_, index) {
+                              return Padding(
+                                  padding: const EdgeInsets.only(left: 10,right: 10,top: 8,bottom: 8),
+                                  child: GestureDetector(
+                                    onTap: () {
+                                      setState(() {
+                                        inx = index.obs;
+                                        tappedListIndex = index;
+                                        print(inx);
+                                        print(data[inx.value][3].toString());
+                                      });
+                                    },
+                                    child: Container(
+                                      height: 55,
+                                      decoration: BoxDecoration(
+                                          border: Border.all(
+                                              color: Colors.black, width: 3),
+                                          color: tappedListIndex == index? Color.fromRGBO(126, 212, 230, 1) :Colors.yellowAccent.shade700,
+                                          borderRadius:
+                                              BorderRadius.circular(8)),
+                                      // color: isSelected
+                                      //     ? Colors.cyan
+                                      //     : Colors.lightGreen,
+                                      child: GestureDetector(
+                                        onTap: () {
+                                          isSelected = true;
+                                            setState(() {
+                                              inx = index.obs;
+                                              tappedListIndex = index;
+                                              print(inx);
+                                              print(data[inx.value][3].toString());
+                                            });
+
+                                        },
+                                        child: Padding(
+                                          padding: const EdgeInsets.all(8.0),
+                                          child: Row(
+                                            mainAxisAlignment:
+                                                MainAxisAlignment.spaceBetween,
+                                            children: [
+                                              Container(
+                                                width: 20,
+                                              ),
+                                              Row(
+                                                children: [
+                                                  Text(
+                                                    data[index][1].toString(),
+                                                    style: TextStyle(
+                                                        fontSize: 17,
+                                                        fontWeight:
+                                                            FontWeight.w500),
+                                                  ),
+                                                  Text("  --  "),
+                                                  Container(
+                                                    width: 5,
+                                                  ),
+                                                  SizedBox(
+                                                    width: 120,
+                                                    child: Text(
+                                                      data[index][2].toString(),
+                                                      maxLines:
+                                                          1, // Don't wrap at all
+                                                      softWrap:
+                                                          false, // Don't wrap at soft breaks
+                                                      overflow:
+                                                          TextOverflow.ellipsis,
+                                                      style: TextStyle(
+                                                          fontSize: 17,
+                                                          fontWeight:
+                                                              FontWeight.w500),
+                                                    ),
+                                                  ),
+                                                ],
+                                              ),
+                                              Obx(
+                                                () => GestureDetector(
+                                                  onTap: () {
+                                                    tappedIndex = index;
+                                                    favorite.toggle();
+                                                    print('favorite');
+                                                  },
+                                                  child: Container(
+                                                    child: Icon(
+                                                      Icons
+                                                          .favorite,
+                                                      color: favorite.isTrue &&
+                                                              tappedIndex ==
+                                                                  index
+                                                          ? Colors.red
+                                                          : Colors.grey,
+                                                    ),
+                                                  ),
+                                                ),
+                                              )
+                                            ],
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                  ));
+                            }),
+                      )
+                    : Center(
+                        child: CircularProgressIndicator(),
+                      )),
+          ),
         ],
       ),
     );
